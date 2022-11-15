bd4ebb9732d4e448d356086fa6ff56db75581c44
GiorgiaAuroraAdorni <adornigiorgia@outlook.it>
2022-11-15 15:13:53 +0100

    Updated script

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-java -jar itas-1.0.jar "data/JCOMSS/questions-skill_model1a.xlsx" "data/JCOMSS/student-answers.xlsx" "data/JCOMSS/results_model1a.xlsx"
+java -jar target/itas-1.0.jar "data/JCOMSS/questions-skill_model1a.xlsx" "data/JCOMSS/student-answers.xlsx" "data/JCOMSS/results_model1a.xlsx"
 
-java -jar itas-1.0.jar "data/JCOMSS/questions-skill_model2a.xlsx" "data/JCOMSS/student-answers.xlsx" "data/JCOMSS/results_model2a.xlsx"
+java -jar target/itas-1.0.jar "data/JCOMSS/questions-skill_model2a.xlsx" "data/JCOMSS/student-answers.xlsx" "data/JCOMSS/results_model2a.xlsx"
 
-java -jar itas-1.0.jar "data/CAT_COMMANDS/questions-skill_model1b.xlsx" "data/CAT_COMMANDS/student-answers-commands.xlsx" "data/CAT_COMMANDS/results_model1b.xlsx"
+java -jar target/itas-1.0.jar "data/CAT_COMMANDS/questions-skill_model1b.xlsx" "data/CAT_COMMANDS/student-answers-commands.xlsx" "data/CAT_COMMANDS/results_model1b.xlsx"
 
-java -jar itas-1.0.jar "data/CAT_COMMANDS/questions-skill_model2b.xlsx" "data/CAT_COMMANDS/student-answers-commands.xlsx" "data/CAT_COMMANDS/results_model2b.xlsx"
+java -jar target/itas-1.0.jar "data/CAT_COMMANDS/questions-skill_model2b.xlsx" "data/CAT_COMMANDS/student-answers-commands.xlsx" "data/CAT_COMMANDS/results_model2b.xlsx"
